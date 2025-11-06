b2a05589c0aac6971f517b0402e48b417e5e3a8b
Sarah Timann <saol0003@stud.kea.dk>
2025-11-06 10:19:13 +0100

    Update Next.js configuration and enhance PetList component with image rendering

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placecats.com",
+        pathname: "/neo/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/components/PetList.jsx
+++ src/app/components/PetList.jsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const PetList = () => {
   return (
     <Link href="/detalje">
       <div className="rounded-2xl p-2 shadow-md">
+        <Image
+          loading="eager"
+          alt="Pet"
+          src="https://placecats.com/neo/300/200"
+          width={300}
+          height={200}
+          className="-mx-2 -mt-2 mb-2 w-[calc(100%+1rem)] max-w-none rounded-t-2xl"
+        />
         <div className="font-semibold tracking-wide font-stretch-extra-condensed">
           Kælenavn
         </div>
