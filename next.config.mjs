86b4a951ec7f75a66df67e9346387a5be30f4cd6
Sarah Timann <saol0003@stud.kea.dk>
2025-11-06 10:34:43 +0100

    Update Next.js configuration to allow broader image pathname and remove unnecessary Link wrapper in Home component

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "placecats.com",
-        pathname: "/neo/**",
+        pathname: "/**",
       },
     ],
   },
--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,12 @@ import PetList from "./components/PetList";
 
 export default function Home() {
   return (
-    <Link href="/detalje">
+    <>
       <section className="mt-20 flex h-13"></section>
       <section className="mt-40 grid grid-cols-2 place-items-center justify-items-stretch gap-4 p-5">
         <PetList />
       </section>
       <nav className="h-20fixed right-0 bottom-0 left-0"></nav>
-    </Link>
+    </>
   );
 }
